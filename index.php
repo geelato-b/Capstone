bf2d006c101de359f203df8b5299c668ecd6f1ba
Angelica Mae Bonganay <81722679+geelato-b@users.noreply.github.com>
2021-06-04 15:24:51 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BUPC CSC Accountability System</title>
+   <!-- CSS only -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" > 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/style.css">
+   
+</head>
+<body>
+<section id= "nav-bar">
+<header>
+
+    <div class="px-3 py-2 ">
+      <div class="container">
+        <div class="navbar d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <a href="#" class="logo d-flex align-items-center my-2 my-lg-0 me-lg-auto text-decoration-none text-dark">logo</a>
+
+          <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+            <li>
+              <a href="#" class="nav-link text-dark">Home</a>
+            </li>
+            <li>
+              <a href="#" class="nav-link text-dark">Status</a>
+            </li>
+            <li>
+              <a href="#" class="nav-link text-dark">About Us</a>
+            </li>
+            <li>
+              <a href="#" class="nav-link text-dark">Setting</a>
+            </li>
+            <li>
+              <a href="#" type="button" class="nav-link btn btn-outline-warning text-dark">Sign In</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+
+</section>
+
+<!-------------------banner section------------------->
+
+<section id="banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <p class="banner-title">BUPC CSC Accountability System</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus sed, quo, 
+                    odit ratione nobis, commodi dignissimos reiciendis 
+                    ipsam voluptates optio delectus consequatur 
+                    excepturi tenetur officia recusandae illo quia 
+                     distinctio?</p>
+            </div>
+            <div class="col-md-6">
+                <img src="img/banner1.png" alt="" class="img-fluid">
+            </div>
+        </div>
+    </div>
+        <div>
+        <img src="img/wave2.png" alt="" class="bottom-img">
+        </div>
+
+       
+
+</section>
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" ></script>
+</body>
+</html>
