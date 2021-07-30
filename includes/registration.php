f04e2ed09adf4e4008102b4d046d7d02a98f52d3
Angelica Mae Bonganay <81722679+geelato-b@users.noreply.github.com>
2021-07-30 12:35:22 +0800

    Update registration.php

--- includes/registration.php
+++ includes/registration.php
@@ -1,9 +1,10 @@
 <?php
-session_start();
+ 
 include_once "func.inc.php";
 
 $stid = $_POST['stid'];
 $studname = $_POST['studname'];
+$bu_email = $_POST['bu_email'];
 $psword = $_POST['psword'];
 $cpassword = $_POST['cpassword'];
 $studprog = $_POST['studprog'];
@@ -28,36 +29,36 @@ die("Connection failed: " . mysqli_connect_error());
  
 if(cidExists($conn, $stid)!== false){
     $_SESSION['status'] = "Account already exist.";
-    header("location:../admin/form.php?error=This Student ID already have an account.");
+    header("location: ../form.php?error=This Student ID already have an account.");
     exit();
 
 }
 if(passExists($conn, $psword)!== false){
     $_SESSION['status'] = "Password already exist.";
-    header("location:../admin/form.php?error=Password is already taken");
+    header("location: ../form.php?error=Password is already taken");
     exit();
 
 }
 if(passMatch($psword, $cpassword)!== false){
     
     $_SESSION['status'] = "Password don't match.";
-    header("location:../admin/form.php?error=Password don't match");
+    header("location: ../form.php?error=Password don't match");
     exit();    
     
 }
 else{
 
-$sql = "INSERT INTO `student_acc` ( `stud_id`, `stud_name`, `password`, `status`, `user_type`)
-VALUES (  '${stid}',  '${studname}','${psword}', 'Active', 'S');" ;
+$sql = "INSERT INTO `student_acc` ( `stud_id`,`bu_email`, `stud_name`, `password`, `status`, `user_type`)
+VALUES (  '${stid}','${bu_email}',  '${studname}','${psword}', 'Active', 'S');" ;
 
 $sql .="INSERT INTO  `student_info` 
-(  `stud_id`, `stud_name`,`stud_program`, `stud_year_block`, `gender`, `stud_birthdate`, `stud_address`) 
- VALUES('${stid}', '${studname}', '${studprog}', '${studyrblck}', '${gender}', '${studbdate}', '${studaddress}');";
+(  `stud_id`,`bu_email`, `stud_name`,`stud_program`, `stud_year_block`, `gender`, `stud_birthdate`, `stud_address`) 
+ VALUES('${stid}','${bu_email}', '${studname}', '${studprog}', '${studyrblck}', '${gender}', '${studbdate}', '${studaddress}');";
 
 
 if (mysqli_multi_query($conn, $sql)) {
      $_SESSION['status'] = "<h2>Successfully Registered.</h2>";
-    header("location:../admin/form.php?success");
+    header("location: ../form.php?success");
     } else {
     echo "Error: " . $sql . mysqli_error($conn);
     }
