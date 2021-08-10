5678367b6dd0c54a89d868b806163d282f12ba2e
Angelica Mae Bonganay <81722679+geelato-b@users.noreply.github.com>
2021-08-10 17:03:16 +0800

    Update update_stat.php

--- includes/update_stat.php
+++ includes/update_stat.php
@@ -1,19 +1,20 @@
 <?php
-if(isset($_POST['stud_id'])){
+if(isset($_POST['gcash_id'])){
         include "db_conn.php";
-        $stud_id = htmlentities($_POST['stud_id']);
-        $new_item_status = htmlentities($_POST['new_item_status']);
+        $gcash_id = htmlentities($_POST['gcash_id']);
+        $new_gc_status = htmlentities($_POST['confirm_status']);
     
          $sql_upd = "UPDATE `gcash`
                         SET gc_status = ?
-                    WHERE stud_id = ?;";
+                    WHERE gcash_id = ?;";
         $stmt_upd = mysqli_stmt_init($conn);
         if(!mysqli_stmt_prepare($stmt_upd, $sql_upd)){
-        header("location: ../admin/Gcash.php?error=8"); //update failed
+        header("location: ../admin/GCash.php?error=8"); //update failed
         exit();
         }
-        mysqli_stmt_bind_param($stmt_upd,"ss",$new_item_status, $stud_id);
+        mysqli_stmt_bind_param($stmt_upd,"ss", $new_gc_status, $gcash_id);
         mysqli_stmt_execute($stmt_upd);
-        header("location: ../admin/Gcash.php?success_update=1");
+        header("location: ../admin/GCash.php?success_update=1");
         
     }
+
