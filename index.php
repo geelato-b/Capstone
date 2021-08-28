7f69946c0b072bd2ffd541c8cd3f58f9a3180915
Angelica Mae Bonganay <81722679+geelato-b@users.noreply.github.com>
2021-08-28 15:00:23 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -25,65 +25,68 @@ include_once ('includes/func.inc.php');
 
 <section id="sign_in">
     <div class="container">
-    
-        <div class="form-container">
+    <div class="row">
+    <div class="form-container">
                     
-            <div class="sign-in">
-            <form action="includes/processlogin.php" method="POST" id="login" class="signin-form" >
-                <?php
-                    if (isset($_SESSION['status'])) {
-                    ?>
-                    <div class="input-box">
-                    <div class="alert alert-success" role="alert">
-                    <?php echo $_SESSION['status']; ?>
-                    </div>
-                    </div>
-                    <?php
-                        
-                        unset($_SESSION['status']);
-                    }
-                    ?>
-            <h2 class="title">Sign In</h2>
-            <div>
-                <img class="img-profile " src="img/logo2.png" width="115px" height="105px">
-                <img class="img-profile" src="img/logo1.png" width="100px" height="100px">
-              </div>
-                    <div class="input-field">
-                    <i class="bi bi-person-fill"></i>
-                        <input name="stud_id" type="text" placeholder="Student ID" required>
-                    </div>
-
-                    <div class="input-field">
-                    <i class="bi bi-lock-fill"></i>
-                        <input  name="password" type="password" placeholder="Password" required>
-                    </div>
-
-                        <button type="submit" name="login"value="login" class="signin-btn">Log In</button>
-                        <br>
-                        <div>
-                        </div>
-                        
-                        <div class="sign_up">
-                        <a href="form.php">Don't have an account yet?</a>
-                        </div>
-                        
-                        
-                    </form>
-            </div>
-        </div>
+                    <div class="sign-in">
+                    <form action="includes/processlogin.php" method="POST" id="login" class="signin-form" >
+                        <?php
+                            if (isset($_SESSION['status'])) {
+                            ?>
+                            <div class="input-box">
+                            <div class="alert alert-success" role="alert">
+                            <?php echo $_SESSION['status']; ?>
+                            </div>
+                            </div>
+                            <?php
+                                
+                                unset($_SESSION['status']);
+                            }
+                            ?>
+                    <h2 class="title">Sign In</h2>
+                    <div>
+                        <img class="img-profile " src="img/logo2.png" width="115px" height="105px">
+                        <img class="img-profile" src="img/logo1.png" width="100px" height="100px">
+                      </div>
+                            <div class="input-field">
+                            <i class="bi bi-person-fill"></i>
+                                <input name="stud_id" type="text" placeholder="Student ID" required>
+                            </div>
         
+                            <div class="input-field">
+                            <i class="bi bi-lock-fill"></i>
+                                <input  name="password" type="password" placeholder="Password" required>
+                            </div>
+        
+                                <button type="submit" name="login"value="login" class="signin-btn">Log In</button>
+                                <br>
+                                <div>
+                                </div>
+                                
+                                <div class="sign_up">
+                                <a href="form.php">Don't have an account yet?</a>
+                                </div>
+                                
+                                
+                            </form>
+                    </div>
+                </div>
+                
+        
+                <div class="panels-container">
+                    <div class="panel left-panel">
+                        <img src="img/sign_in.svg" class="image" alt="">
+                    </div>
+                </div>
 
-        <div class="panels-container">
-            <div class="panel left-panel">
-            
-                <img src="img/sign_in.svg" class="image" alt="">
-            </div>
-        </div>
+    </div>
+        
 
     </div>
 
 
 </section>
+
 
 
 
