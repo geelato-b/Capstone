a2de5ef7a4f65c73f1d766e1f2084692c0d9aeb9
Angelica Mae Bonganay <81722679+geelato-b@users.noreply.github.com>
2021-09-04 18:15:47 +0800

    Update AddAcc.php

--- includes/AddAcc.php
+++ includes/AddAcc.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if(isset($_POST['accbty_desc'])){
-include_once "db_conn.php";
+include_once "../includes/db_conn.php";
    
     $accbty_name = htmlentities($_POST['accbty_name']);
     $accbty_price = htmlentities($_POST['accbty_price']);
@@ -11,15 +11,15 @@ include_once "db_conn.php";
     $sql_check="SELECT * 
                 FROM `accountabilities`
                  WHERE  accbty_name = ?
-                  ;";
+                 AND accbty_deadline = ? ;";
     
     $stmt_chk = mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt_chk, $sql_check)){
         $_SESSION['status1'] = "Connection Failed.";
-       header("location: ../admin/update.php?error=3"); //statement failed
+       header("location: update.php?error=3"); //statement failed
         exit();
     }
-    mysqli_stmt_bind_param($stmt_chk, "s", $accbty_name);
+    mysqli_stmt_bind_param($stmt_chk, "ss", $accbty_name, $accbty_deadline);
     mysqli_stmt_execute($stmt_chk);
     $chk_result=mysqli_stmt_get_result($stmt_chk);
     $arr=array();
@@ -28,8 +28,8 @@ include_once "db_conn.php";
     }
 
     if(!empty($arr)){
-        $_SESSION['status1'] = "<b>Already Exist.</b><br> Please change the item name.";
-        header("location: ../admin/update.php?error=1&itemname={$accbty_name}"); //item exist
+        $_SESSION['status1'] = "Already Exist.";
+        header("location: update.php?error=1&itemname={$accbty_name}"); //item exist
         exit();
     }
     else{
@@ -42,12 +42,12 @@ include_once "db_conn.php";
         $stmt_ins = mysqli_stmt_init($conn);
         if(!mysqli_stmt_prepare($stmt_ins, $sql_ins)){
             $_SESSION['status1'] = "Failed to insert. Try again.";
-            header("location: ../admin/update.php?error=2"); //insert failed
+            header("location: update.php?error=2"); //insert failed
             exit();
         }
         mysqli_stmt_bind_param($stmt_ins, "ssss", $accbty_name,  $accbty_price,  $accbty_desc, $accbty_deadline);
         mysqli_stmt_execute($stmt_ins);
         $_SESSION['status'] ="<b>".  $accbty_name . "</b>". " is successfully added. <br><b>Deadline: </b>". $accbty_deadline;
-        header("location: ../admin/update.php?error=0"); //success
+        header("location: update.php?error=0"); //success
     }
 }
